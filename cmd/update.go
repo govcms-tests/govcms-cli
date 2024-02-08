36f8c427e05db43b764f2a240a7ff9134963cf61
Joseph Zhao <pandaski@outlook.com.au>
2024-02-08 20:06:05 +1100

    Init the project

--- cmd/update.go
+++ cmd/update.go
@@ -1,5 +1,4 @@
-The MIT License (MIT)
-
+/*
 Copyright © 2024 Joseph Zhao pandaski@outlook.com.au
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,36 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/govcms-tests/govcms-cli/utils/docker"
+	"github.com/spf13/cobra"
+)
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Pulls Docker images and recreates the containers",
+	Long:  "Pulls Docker images and recreates the containers.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// List of Docker image names to pull
+		imageList := []string{"govcmsextras/dnsmasq", "adminer", "mariadb:lts", "mailhog/mailhog"}
+		// Iterate over the image list
+		for _, imageName := range imageList {
+			err := docker.DockerImagePull(imageName)
+			if err != nil {
+				fmt.Printf("Error pulling image %s: %v\n", imageName, err)
+				continue // Continue to the next image on error
+			}
+			fmt.Printf("Successfully pulled image: %s\n", imageName)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
