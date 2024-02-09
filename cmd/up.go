d3588121c3cde25c0a432dd31465d6354b495741
jackwrfuller <jack.fuller101@gmail.com>
2024-02-09 15:33:21 +1100

    Investigate 'up' command

--- cmd/up.go
+++ cmd/up.go
@@ -1,38 +1,36 @@
-/*
-Copyright © 2024 Joseph Zhao pandaski@outlook.com.au
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
-	"github.com/govcms-tests/govcms-cli/pkg/govcms"
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
 )
 
-// upCmd represents the up command
+type saveOutput struct {
+	savedOutput []byte
+}
+
+func (so *saveOutput) Write(p []byte) (n int, err error) {
+	so.savedOutput = append(so.savedOutput, p...)
+	return os.Stdout.Write(p)
+}
+
 var upCmd = &cobra.Command{
 	Use:   "up",
-	Short: "Initiates the GovCMS local development environment",
-	Long:  "Initiates the GovCMS local development environment.",
-	Run: func(cmd *cobra.Command, args []string) {
-		govcms.Start()
+	Short: "Launch docker container",
+	Run: func(cmd *cobra.Command, arg []string) {
+		var so saveOutput
+
+		command := exec.Command("/bin/sh", "-c", "docker compose up -d")
+		command.Dir = "govcms/distribution"
+
+		command.Stdin = os.Stdin
+		command.Stdout = &so
+		command.Stderr = os.Stderr
+
+		_ = command.Run()
+		fmt.Printf("%s", so.savedOutput)
 	},
 }
 
