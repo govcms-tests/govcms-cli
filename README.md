6dfabddf9af1dade23d00dcd6daed993ab14b0c7
jackwrfuller <jack.fuller101@gmail.com>
2024-03-12 14:21:14 +1100

    remove ahoy and pygmy requirements for now

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # govcms-cli
 GovCMS's command line tool for local development.
+
+# Installation
+
+```shell
+brew tap govcms-tests/govcms-cli
+brew install govcms-cli
+```
--- pkg/govcms/requirements.go
+++ pkg/govcms/requirements.go
@@ -14,12 +14,12 @@ func CheckRequirements() error {
 	if err := checkGit(); err != nil {
 		errors = append(errors, err)
 	}
-	if err := checkAhoy(); err != nil {
-		errors = append(errors, err)
-	}
-	if err := checkPygmy(); err != nil {
-		errors = append(errors, err)
-	}
+	//if err := checkAhoy(); err != nil {
+	//	errors = append(errors, err)
+	//}
+	//if err := checkPygmy(); err != nil {
+	//	errors = append(errors, err)
+	//}
 
 	if len(errors) > 0 {
 		return errors[0] // Returning just the first error for simplicity
@@ -34,8 +34,9 @@ func checkDocker() error {
 func checkGit() error {
 	return checkCommand("git", "--version")
 }
-func checkAhoy() error  { return checkCommand("ahoy", "--version") }
-func checkPygmy() error { return checkCommand("pygmy", "--version") }
+
+//func checkAhoy() error  { return checkCommand("ahoy", "--version") }
+//func checkPygmy() error { return checkCommand("pygmy", "--version") }
 
 func checkCommand(command string, args ...string) error {
 	cmd := exec.Command(command, args...)
