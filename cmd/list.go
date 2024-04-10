5a9e9f7f951a9ef19b3e39f803753f6764e2c615
Tara <tarawijayagunawarana@192-168-1-117.tpgi.com.au>
2024-04-10 13:28:13 +1000

    [GC-27] Implement the pretty printing of govcms list

--- cmd/list.go
+++ cmd/list.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 var listCmd = &cobra.Command{
@@ -12,9 +13,34 @@ var listCmd = &cobra.Command{
 	Long:  "List all locally installed GovCMS installations",
 	Run: func(cmd *cobra.Command, args []string) {
 		installationManager.Sync()
-		fmt.Println("Found the following local instances:")
-		fmt.Println("============================")
 		paths, _ := installationManager.GetAllPaths()
-		fmt.Println(strings.Join(paths, "\n"))
+
+		// Render the table
+		renderTable(paths)
 	},
+}
+
+// Function to render a table with the provided paths
+func renderTable(paths []string) {
+	// Initialise a new table
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.SetStyle(table.StyleColoredDark)
+
+	// Define column headers
+	t.SetTitle("Found the following local instances:")
+	t.AppendHeader(table.Row{"#", "Path"})
+
+	// Append rows to the table
+	for i, path := range paths {
+		// Add cells to the row with style for the first column
+		row := table.Row{
+			i + 1, // Set style for the first column
+			path,
+		}
+		t.AppendRow(row)
+	}
+
+	// Render the table
+	t.Render()
 }
